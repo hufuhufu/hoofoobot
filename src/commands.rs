f08411895bdfb3a9cc781489b7716f6e1fa1d292
Arif Hermawan <arif.hermawan12@gmail.com>
2023-09-21 12:21:43 +0800

    feat: added graveyard command

--- src/commands.rs
+++ src/commands.rs
@@ -6,3 +6,35 @@ pub async fn hello(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("world!").await?;
     Ok(())
 }
+
+/// Move this channel to graveyard.
+#[poise::command(slash_command, prefix_command, guild_only)]
+pub async fn graveyard(ctx: Context<'_>) -> Result<(), Error> {
+    let data = ctx.data();
+    let channel_id = ctx.channel_id();
+
+    let Some(guild_id) = ctx.guild_id() else { 
+        ctx.say("You need to run this command in a guild!").await?;
+        return Ok(());
+    };
+    let Some(config) = data.get_config(guild_id) else {
+        data.insert_config(guild_id, Default::default());
+        ctx.say("Please set up a graveyard category.").await?;
+        return Ok(());
+    };
+    let Some(graveyard) = config.get_graveyard() else {
+        ctx.say("Please set up a graveyard category.").await?;
+        return Ok(());
+    };
+
+    channel_id
+        .edit(ctx, |c| {
+            c.category(graveyard);
+            c
+        })
+        .await?;
+
+    ctx.send(|reply| reply.content("Channel moved!").ephemeral(true)).await?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
+use std::{collections::HashMap, sync::Mutex};
+
 use anyhow::Context as _;
-use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::{self as serenity, ChannelId, GuildId};
 use shuttle_poise::ShuttlePoise;
 use shuttle_secrets::SecretStore;
 use tracing::info;
 
-pub struct Data {} // User data, which is stored and accessible in all command invocations
+#[derive(Debug, Default)]
+pub struct Data {
+    config: Mutex<HashMap<GuildId, Config>>,
+}
+
+impl Data {
+    pub fn insert_config(&self, guild_id: GuildId, conf: Config) {
+        let mut config = self.config.lock().unwrap();
+        config.insert(guild_id, conf);
+    }
+    pub fn get_config(&self, guild_id: GuildId) -> Option<Config> {
+        let configs = self.config.lock().unwrap();
+        configs.get(&guild_id).copied()
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, Hash)]
+pub struct Config {
+    graveyard: Option<ChannelId>,
+}
+
+impl Config {
+    pub fn set_graveyard(&mut self, id: ChannelId) {
+        self.graveyard = Some(id);
+    }
+    pub fn get_graveyard(&self) -> Option<ChannelId> {
+        self.graveyard
+    }
+}
+
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
@@ -19,7 +50,7 @@ async fn poise(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> Shuttle
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![commands::hello()],
+            commands: vec![commands::hello(), commands::graveyard()],
             prefix_options: poise::PrefixFrameworkOptions {
                 prefix: Some("f:".into()),
                 additional_prefixes: vec![poise::Prefix::Literal("F:")],
@@ -46,7 +77,14 @@ async fn poise(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> Shuttle
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+
+                let mut config = Config::default();
+                config.set_graveyard(692419154971459736.into());
+
+                let data = Data::default();
+                data.insert_config(692419154971459734.into(), config);
+
+                Ok(data)
             })
         })
         .build()
