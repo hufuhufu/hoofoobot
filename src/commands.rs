2866c2b6b988034c51528d5c38173c587f8d6aee
Arif Hermawan <arif.hermawan12@gmail.com>
2023-10-20 20:31:28 +0800

    feat: reply from set_afk_channel command

--- src/commands.rs
+++ src/commands.rs
@@ -84,11 +84,20 @@ pub async fn incr_score(
 #[poise::command(slash_command, prefix_command, guild_only)]
 pub async fn set_afk_channel(
     ctx: Context<'_>,
-    #[description = "AFK channel"] #[rename = "channel"] afk_channel_id: ChannelId,
+    #[description = "AFK channel"]
+    #[rename = "channel"]
+    afk_channel_id: ChannelId,
 ) -> Result<(), Error> {
     let guild_id = ctx.guild_id().unwrap();
-    
+
     Configs::set_afk_channel(ctx.data(), guild_id, afk_channel_id).await?;
+
+    ctx.send(|reply| {
+        reply
+            .content(format!("AFK channel id set to {}", afk_channel_id.0))
+            .ephemeral(true)
+    })
+    .await?;
 
     Ok(())
 }
